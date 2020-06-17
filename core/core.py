31f9f0d31206c3aba42a87b8aafef59fa86c4214
root <root@LAPTOP-F5HU9JVM.localdomain>
2020-06-17 16:58:19 -0700

    finishing the calculate3DCoordinate function of core.py

--- core/core.py
+++ core/core.py
@@ -1,12 +1,8 @@
 import numpy as np
 import json
 import math
-<<<<<<< HEAD
 import cv2
-from depth_model import pred_depth
-=======
-from cv_model import pred
->>>>>>> 279476bb3717c72f5d1a422291b8b74f68900f11
+import pred
 
 from torchvision import transforms, datasets
 
@@ -27,41 +23,36 @@ def runCVModel(image_path):
     
     """Function to predict for a single image or folder of images
     """
-    depth, boxes_output = pred.predict(image_path)
-    return depth, boxes_output
+    depth, output = pred.predict(image_path)
+    return depth, output
 
-<<<<<<< HEAD
-def getBoundingBoxes(image_path):
-	#Ravit will do
-	#Goal: Take an image and run inference to get bounding boxes
-	#https://github.com/tensorflow/models/tree/master/research/object_detection
-    return
 
-def getPixelDepth(pixel_coordinate_x, pixel_coordinate_y, img, pixel_radius=10):
-=======
 def getPixelDepth(pixel_coordinate_x, pixel_coordinate_y, depth_map, img, pixel_radius=10):
->>>>>>> 279476bb3717c72f5d1a422291b8b74f68900f11
 	#Santript, for you
 	#Goal: Apply the algorithm we were discussing on finding the depth
 	#	take a circle of radius pixel_radius
 	#	find 50% of values of depth distribution centered at the given pixel
 	#	later, we'll incorporate same solution with masks
     
-    allDepth = getDepthMap('test_image.jpg')
+    allDepth = runCVModel('test_image.jpg')
     
+    #finds all the pixel coordinates of an image
     img1 = cv2.imread(img,0)
     indices = np.where(img1 != [0])
     coordinates = zip(indices[0], indices[1])
     
+    #extracts all the pixels that are within a 10 pixel radius from the center pixel coordinate
     neededPixels = []
     for (pixel_x, pixel_y) in coordinates:
         if (pixel_x - pixel_coordinate_x)**2 + (pixel_y - pixel_coordinate_y)**2 <= (pixel_radius**2):
                 neededPixels.append(pixel_x,pixel_y)
-    
+                
+    #gets the depth of neededPixels using depth estimation
     allPixelDepth = []
     for pixels in neededPixels:
         allPixelDepth.append(allDepth[pixels])
-    
+        
+    #finds the final needed depth
     length = len(allPixelDepth)
     allPixelDepth.sort()
     neededLength = int(length/2)
@@ -116,14 +107,67 @@ def calculate3DCoordinates(depthMap, pixelCoordinate):
     bottom_hypotenuse = math.sqrt(top_pixel_depth**2 - camera_height**2)
     """
     
-    with open(json_path,"r") as f:
-		neededVars = json.load(f)
+    #array of positions needed for pose estimation
+    COCO_PERSON_KEYPOINT_NAMES = [
+    'nose',
+    'left_eye',
+    'right_eye',
+    'left_ear',
+    'right_ear',
+    'left_shoulder',
+    'right_shoulder',
+    'left_elbow',
+    'right_elbow',
+    'left_wrist',
+    'right_wrist',
+    'left_hip',
+    'right_hip',
+    'left_knee',
+    'right_knee',
+    'left_ankle',
+    'right_ankle'
+]
     
+    with open("sample_config.json","r") as f:
+    		neededVars = json.load(f)
+    
+    #all necessary variables from json file
     center = neededVars["calibration"]["centerpoint"]
-
-	
+    v = neededVars["calibration"]["verticalAngle"]
+    height = neededVars["calibration"]["cameraHeight"]
+    
+    #finds the pixel coordinate of the nose of a person(nose just for now. It might change)
+    depth,output = runCVModel("test_image.jpg")
+    pixelCoordinate = depth.output["keypoints"][:][COCO_PERSON_KEYPOINT_NAMES.index("nose")]
+    
+    #finds the offset of object from center of image(distance)
+    offset = pixelCoordinate-center
+    
+    #calibration constant
+    k = calculateCalibrationConstant("sample_config.json")
     
     
+    #needed to find the camera direction vector(how much camera has to move to have a direct view of object)
+    delta_x = k * math.atan(offset[0])
+    delta_y = k * math.atan(offset[1])
+    
+    #3D direction vector
+    3DdirectionVector = [math.tan(delta_x),1,math.tan(delta_y - v)]
+    
+    #finds the magnitude of the 3D direction vector(needed for normalization)
+    magnitude = math.sqrt(3DdirectionVector[0]**2 + 3DdirectionVector[1]**2 + 3DdirectionVector[2]**2)
+    
+    #uses magnitude to normalize the 3D direction vector
+    normalized = 3DdirectionVector/magnitude
+    
+    #finds depth of pixelCoordinate using the getPixelDepth function(needed to find the actual 3D coordinates of object)
+    depth = getPixelDepth(pixelCoordinate)
+    
+    #camera's 3D coordinates IRL
+    camera_position = [0,0,height]
+    
+    #3D coordinates of object
+    actual3DCoords = depth * normalized + camera_position
     
     
-    return
+    return actual3DCoords
