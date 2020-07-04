cacc5ed4e03ce1e27ff1cb26f5a9c0d8a5d7a382
Ravit Sharma <sharma.ravit@gmail.com>
2020-07-04 12:07:01 -0700

    Revising recent changes. Improving visualization.

--- core/utils.py
+++ core/utils.py
@@ -5,9 +5,7 @@ import cv_model.utils as cv_utils
 def makeVisualizationOutput(pm, output, distance_threshold=2, score_threshold = 0.80):
 	coords3D = []
 	long_lats = []
-	wearingMasks = []
-	faceBoxes = []
-	boxes = []
+	masked = []
 	for i in range(len(output["boxes"])):
 		#print("Boxes", output["boxes"][i])
 		if (output["scores"][i] < score_threshold):
@@ -22,8 +20,6 @@ def makeVisualizationOutput(pm, output, distance_threshold=2, score_threshold = 
 		#0=unsure, 1=wearing, 2=not wearing
 		wearingMask = 0
 		#print(output["masks"])
-		faceBoxes_subList = []
-		boxes_subList = []
 		for maskOut in output["masks"]:
 			face_box = maskOut[0]
 			mask = maskOut[1]
@@ -44,29 +40,23 @@ def makeVisualizationOutput(pm, output, distance_threshold=2, score_threshold = 
 
 		long_lats.append(long_lat)
 		coords3D.append(coord3D)
-		wearingMasks.append(wearingMask)
-		faceBoxes.append(np.array(faceBoxes_subList).tolist())
-		boxes.append(np.array(boxes_subList).tolist())
+		masked.append(wearingMask)
 
 	if (coords3D != []):
 		long_lats = np.swapaxes(np.asarray(long_lats), 0, 1).tolist()
 		coords3D = np.asarray(coords3D).tolist()
-	if (faceBoxes != []):
-		faceBoxes = np.asarray(faceBoxes).tolist()
-	if (boxes != []):
-		boxes = np.asarray(boxes).tolist()
 
-	safe = [1] * len(coords3D)
+	distanced = [1] * len(coords3D)
 	for i in range(len(coords3D)):
 		for j in range(i+1, len(coords3D)):
-			if (safe[i] == 0 and safe[j] == 0):
+			if (distanced[i] == 0 and distanced[j] == 0):
 				continue
 			assert len(coords3D[i]) == len(coords3D[j])
 			distance = math.sqrt(sum(e**2 for e in [coords3D[i][k]-coords3D[j][k] for k in range(2)]))
 			if (distance < distance_threshold):
-				safe[i] = 0
-				safe[j] = 0
-	#print("faceBoxes:", faceBoxes)
-	#print("boxes:", boxes)
-	predOutput = {"3DCoordinates":coords3D, "lat-long":long_lats,"Boxes":boxes, "Faces":faceBoxes, "wearingMasks":wearingMasks, "safe":safe}
+				distanced[i] = 0
+				distanced[j] = 0
+		if 1 not in distanced:
+			break
+	predOutput = {"3DCoordinates":coords3D, "lat-long":long_lats, "masked":masked, "distanced":distanced}
 	return predOutput
--- visualization/browser-demo/drawPlots.js
+++ visualization/browser-demo/drawPlots.js
@@ -1,178 +1,194 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-	   // Typical action to be performed when the document is ready:
-	   var data = JSON.parse(xhttp.responseText);
-	} else {
-		return;
-	}
-	
-	//Safety classification rules are in this exact order:
-	//    if unmasked individual (regardless of distance), dark red individual
-	//    else if individual is masked but distance is critical, bright red individual
-	//    else if unsure about mask, orange individual
-	//    otherwise, mark cyan for safe individual
-	var x_values_safe = [];
-	var y_values_safe = [];
-	var z_values_safe = [];
-	var x_values_unsafe = [];
-	var y_values_unsafe = [];
-	var z_values_unsafe = [];
-	var x_values_mask_unsure = [];
-	var y_values_mask_unsure = [];
-	var z_values_mask_unsure = [];
-	var x_values_unmasked = [];
-	var y_values_unmasked = [];
-	var z_values_unmasked = [];
+var first = true;
 
-	for (let i = 0; i < data['3DCoordinates'].length; i++) {
-		var curr_coordinate = data['3DCoordinates'][i];
-		if (data['wearingMasks'][i] == 2) {
-			x_values_unmasked.push(curr_coordinate[0]);
-			y_values_unmasked.push(curr_coordinate[1]);
-			z_values_unmasked.push(curr_coordinate[2]);
-		}else if (data['safe'][i] == 0) {
-			x_values_unsafe.push(curr_coordinate[0]);
-			y_values_unsafe.push(curr_coordinate[1]);
-			z_values_unsafe.push(curr_coordinate[2]);
-		}else if (data['wearingMasks'][i] == 0) {
-			x_values_mask_unsure.push(curr_coordinate[0]);
-			y_values_mask_unsure.push(curr_coordinate[1]);
-			z_values_mask_unsure.push(curr_coordinate[2]);
-		} else if (data['wearingMasks'][i] == 1) {
-			x_values_safe.push(curr_coordinate[0]);
-			y_values_safe.push(curr_coordinate[1]);
-			z_values_safe.push(curr_coordinate[2]);
+function update() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+		// Typical action to be performed when the document is ready:
+		var data = JSON.parse(xhttp.responseText);
+		} else {
+			return;
 		}
-	}
 
+		document.getElementById("statsTotal").innerHTML = data['3DCoordinates'].length;
+		
+		//Safety classification rules are in this exact order:
+		//    if unmasked individual (regardless of distance), dark red individual
+		//    else if individual is masked but distance is critical, bright red individual
+		//    else if unsure about mask, orange individual
+		//    otherwise, mark cyan for safe individual
+		var safe_values = [[],[],[]];
+		var undistanced_values = [[],[],[]];
+		var unmasked_values = [[],[],[]];
+		var undistanced_unmasked_values = [[],[],[]];
 
+		for (let i = 0; i < data['3DCoordinates'].length; i++) {
+			var curr_coordinate = data['3DCoordinates'][i];
 
+			var unmasked = data['masked'][i] == 2 ? true : false;
+			var undistanced = data['distanced'][i] == 0 ? true : false;
+			
+			if (unmasked && undistanced) {
+				undistanced_unmasked_values[0].push(curr_coordinate[0]);
+				undistanced_unmasked_values[1].push(curr_coordinate[1]);
+				undistanced_unmasked_values[2].push(curr_coordinate[2]);
+			} else if (unmasked) {
+				unmasked_values[0].push(curr_coordinate[0]);
+				unmasked_values[1].push(curr_coordinate[1]);
+				unmasked_values[2].push(curr_coordinate[2]);
+			} else if (undistanced) {
+				undistanced_values[0].push(curr_coordinate[0]);
+				undistanced_values[1].push(curr_coordinate[1]);
+				undistanced_values[2].push(curr_coordinate[2]);
+			} else {
+				safe_values[0].push(curr_coordinate[0]);
+				safe_values[1].push(curr_coordinate[1]);
+				safe_values[2].push(curr_coordinate[2]);
+			}
+		}
 
-	var lat_values = [];
-	var long_values = [];
+		document.getElementById("statsUndistanced").innerHTML = undistanced_values[0].length + undistanced_unmasked_values[0].length;
+		document.getElementById("statsUnmasked").innerHTML = unmasked_values[0].length + undistanced_unmasked_values[0].length;
 
-	//assert length of data['lat-long'][0] == data['lat-long'][1]
-	for (let i = 0; i < data['lat-long'][0].length; i++) {
-		var curr_geolocation = data['lat-long'];
+		var lat_values = [];
+		var long_values = [];
 
-		lat_values.push(curr_geolocation[0][i]);
-		long_values.push(curr_geolocation[1][i]);
-	}
+		//assert length of data['lat-long'][0] == data['lat-long'][1]
+		for (let i = 0; i < data['lat-long'][0].length; i++) {
+			var curr_geolocation = data['lat-long'];
 
-	//==========================
-	var trace_safe = {
-	    x: x_values_safe,
-	    y: y_values_safe,
-	    z: z_values_safe,
-		name: 'safe',
-	    mode: 'markers',
-	    marker: {
-	        size: 8,
-	        line: {
-	            color: 'rgba(0, 139, 139, 0.14)',
-	            width: 0.5
-	        },
-	        opacity: 0.8
-	    },
-	    type: 'scatter3d'
-	};
+			lat_values.push(curr_geolocation[0][i]);
+			long_values.push(curr_geolocation[1][i]);
+		}
 
-	var trace_unsafe = {
-	    x: x_values_unsafe,
-	    y: y_values_unsafe,
-	    z: z_values_unsafe,
-		name: 'unsafe',
-	    mode: 'markers',
-	    marker: {
-	        size: 8,
-	        line: {
-	            color: 'rgba(139, 0, 0, 0.14)',
-	            width: 0.5
-	        },
-	        opacity: 0.8
-	    },
-	    type: 'scatter3d'
-	};
+		//==========================
+		var trace_safe = {
+			x: safe_values[0],
+			y: safe_values[1],
+			z: safe_values[2],
+			name: 'safe',
+			mode: 'markers',
+			marker: {
+				color: 'rgba(0, 100, 0, 0.5)',
+				size: 8,
+				line: {
+					width: 0.5
+				},
+				opacity: 0.8
+			},
+			type: 'scatter3d'
+		};
 
-	var trace_unmasked = {
-	    x: x_values_unmasked,
-	    y: y_values_unmasked,
-	    z: z_values_unmasked,
-		name: 'maskViolation',
-	    mode: 'markers',
-	    marker: {
-	        size: 8,
-	        line: {
-	            color: 'rgba(217, 0, 0, 0.14)',
-	            width: 0.5
-	        },
-	        opacity: 0.8
-	    },
-	    type: 'scatter3d'
-	};
+		var trace_undistanced = {
+			x: undistanced_values[0],
+			y: undistanced_values[1],
+			z: undistanced_values[2],
+			name: 'undistanced',
+			mode: 'markers',
+			marker: {
+				color: 'rgba(255, 150, 0, 0.5)',
+				size: 8,
+				line: {
+					width: 0.5
+				},
+				opacity: 0.8
+			},
+			type: 'scatter3d'
+		};
 
-	var trace_mask_unverified = {
-	    x: x_values_mask_unsure,
-	    y: y_values_mask_unsure,
-	    z: z_values_mask_unsure,
-		name: 'unverifiedMask',
-	    mode: 'markers',
-	    marker: {
-	        size: 8,
-	        line: {
-	            color: 'rgba(217, 217, 217, 0.14)',
-	            width: 0.5
-	        },
-	        opacity: 0.8
-	    },
-	    type: 'scatter3d'
-	};
+		var trace_unmasked = {
+			x: unmasked_values[0],
+			y: unmasked_values[1],
+			z: unmasked_values[2],
+			name: 'unmasked',
+			mode: 'markers',
+			marker: {
+				color: 'rgba(255, 255, 0, 0.5)',
+				size: 8,
+				line: {
+					width: 0.5
+				},
+				opacity: 0.8
+			},
+			type: 'scatter3d'
+		};
 
-	var data1 = [trace_safe, trace_unsafe, trace_unmasked, trace_mask_unverified];
-	var layout = {
-	    margin: {
-	        l: 0,
-	        r: 0,
-	        b: 0,
-	        t: 0
-	    }
-	};
+		var trace_mask_undistanced_unmasked = {
+			x: undistanced_unmasked_values[0],
+			y: undistanced_unmasked_values[1],
+			z: undistanced_unmasked_values[2],
+			name: 'undistanced_unmasked',
+			mode: 'markers',
+			marker: {
+				color: 'rgba(255, 0, 0, 0.5)',
+				size: 8,
+				line: {
+					width: 0.5
+				},
+				opacity: 0.8
+			},
+			type: 'scatter3d'
+		};
 
+		var scatterData = [trace_safe, trace_undistanced, trace_unmasked, trace_mask_undistanced_unmasked];
+		var scatterLayout = {
+			margin: {
+				l: 0,
+				r: 0,
+				b: 0,
+				t: 0
+			}
+		};
+		
 
-	Plotly.newPlot('plotDiv', data1, layout);
-
-	//==========================
-	var data = [{
-	  type:'scattermapbox',
-	  lat:lat_values,
-	  lon:long_values,
-	  mode:'markers',
-	  marker: {
-		size:8
-	  },
-	  text:['Person1', 'Person2']
-	}]
-
-	var layout = {
-	  autosize: true,
-	  hovermode:'closest',
-	  mapbox: {
-		bearing:110,
-		center: {
-		  lat:lat_values[0],
-		  lon:long_values[0]
+		//==========================
+		var mapData = [{
+		type:'scattermapbox',
+		lat:lat_values,
+		lon:long_values,
+		mode:'markers',
+		marker: {
+			size:8
 		},
-		pitch:0,
-		zoom:19
-	  },
-	}
+		text:['Person1', 'Person2']
+		}]
 
-	Plotly.setPlotConfig({
-	  mapboxAccessToken: "pk.eyJ1Ijoic3Jhdml0MSIsImEiOiJja2JzY3NpcHgwMGJnMnZzYTY5ZWsyeDR6In0.CIOWohypCmf_oCzed32xRA"
-	})
+		var layout = {
+		autosize: true,
+		hovermode:'closest',
+		mapbox: {
+			bearing:110,
+			center: {
+			lat:lat_values[0],
+			lon:long_values[0]
+			},
+			pitch:0,
+			zoom:19
+		},
+		margin: {
+			l: 0,
+			r: 0,
+			b: 0,
+			t: 0
+		}
+		}
 
-	Plotly.newPlot('mapDiv', data, layout);
-};
-xhttp.open("GET", "output.json", true);
-xhttp.send();
+		Plotly.setPlotConfig({
+		mapboxAccessToken: "pk.eyJ1Ijoic3Jhdml0MSIsImEiOiJja2JzY3NpcHgwMGJnMnZzYTY5ZWsyeDR6In0.CIOWohypCmf_oCzed32xRA"
+		})
+
+		if (first) {
+			Plotly.newPlot('plotDiv', scatterData, scatterLayout);
+			Plotly.newPlot('mapDiv', mapData, layout);
+			first = false;
+		} else {
+			Plotly.react('plotDiv', scatterData, scatterLayout);
+			Plotly.react('mapDiv', mapData, layout);
+		}
+
+	};
+	xhttp.open("GET", "output.json", true);
+	xhttp.send();
+}
+
+setInterval(update(), 1000);
