4167f32507357033e5fce244afcb1e3e39ec9012
Ravit Sharma <sharma.ravit@gmail.com>
2020-07-09 16:03:53 -0700

    Incorporating add site functionality

--- visualization/index.js
+++ visualization/index.js
@@ -1,3 +1,89 @@
+function showNone() {
+  document.getElementById("dashboard-nav-buttons").style.display = "none";
+  document.getElementById("dashboardPage").style.display = "none";
+  document.getElementById("signup-div-wrapper").style.display = "none";
+  document.getElementById("login-div-wrapper").style.display = "none";
+  document.getElementById("addSite").style.display = "none";
+}
+function loginPage() {
+  showNone();
+  document.getElementById("login-div-wrapper").style.display = "block";
+}
+function signupPage() {
+  showNone();
+  document.getElementById("signup-div-wrapper").style.display = "block";
+}
+function dashboardPage() {
+  showNone();
+  document.getElementById("add-site-btn").style.display = "inline";
+  document.getElementById("dashboard-return-btn").style.display = "none";
+  document.getElementById("dashboard-nav-buttons").style.display = "block";
+  document.getElementById("dashboardPage").style.display = "block";
+}
+function addSitePage() {
+  console.log("add site page");
+  showNone();
+  document.getElementById("add-site-btn").style.display = "none";
+  document.getElementById("dashboard-return-btn").style.display = "inline";
+  document.getElementById("dashboard-nav-buttons").style.display = "block";
+  document.getElementById("addSite").style.display = "flex";
+}
+function showpasswordmain() {
+  var x = document.getElementById("login_password_field");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+function showpasswordsignup() {
+  var x = document.getElementById("signup_password_field");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function login(){
+  var userEmail = document.getElementById("login_email_field").value;
+  var userPass = document.getElementById("login_password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    var loginError = document.getElementById("login-error-msg");
+    loginError.style.display = "block";
+    loginError.innerHTML = "Error: " + errorMessage;
+  });
+}
+function loginGoogle(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    var token = result.credential.accessToken;
+    var user = result.user;
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+  });
+}
+function signup(){
+  //TODO: Add more features to sign up page
+  var userEmail = document.getElementById("signup_email_field").value;
+  var userPass = document.getElementById("signup_password_field").value;
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+}
+function logout(){
+  firebase.auth().signOut();
+}
+
 var lastUpdate = 0;
 
 function initializeDashboard() {
@@ -11,7 +97,7 @@ function initializeDashboard() {
 
     var accessibleEnvironments = JSON.parse(xhr.responseText);
     var sel = document.getElementById('dashboard-select');
-    
+
     //first clearing the child nodes
     while (sel.hasChildNodes())
       sel.removeChild(sel.firstChild);
@@ -21,7 +107,7 @@ function initializeDashboard() {
       var txt = accessibleEnvironments[key];
       var opt = document.createElement('option');
       opt.appendChild( document.createTextNode(txt));
-      opt.value = key; 
+      opt.value = key;
 
       sel.appendChild(opt);
     });
@@ -33,7 +119,6 @@ function initializeDashboard() {
     xhr.send(JSON.stringify({"idtoken":idToken}));
   }).catch(function(error) { console.error(error); });
 }
-
 function updateDashboardArgs(dashboardID) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
@@ -42,11 +127,12 @@ function updateDashboardArgs(dashboardID) {
       return;
     }
 
-    console.log("Success in getting response from Post request to get environment file", xhr.responseText);
     var config = JSON.parse(xhr.responseText);
+    console.log("Success in getting response from Post request to get environment file", config);
     if (config["authorized"] && !config["toDate"]) {
       lastUpdate = config["currentTime"];
-      update(config["dashboard"]);
+      var dashboard = config["dashboard"];
+      update(dashboard);
     }
   }
   xhr.open("POST", "environment", true);
@@ -54,7 +140,6 @@ function updateDashboardArgs(dashboardID) {
     xhr.send(JSON.stringify({"idtoken":idToken, "dashboard":dashboardID, "lastUpdate":lastUpdate}));
   }).catch(function(error) { console.error(error); });
 }
-
 function updateDashboard(forceUpdate=false) {
   var selectObj = document.getElementById("dashboard-select");
   if (selectObj.selectedIndex != -1) {
@@ -65,101 +150,26 @@ function updateDashboard(forceUpdate=false) {
     updateDashboardArgs(dashboardID);
   }
 }
-
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     initializeDashboard();
-
-    document.getElementById("dashboardPage").style.display = "block";
-    document.getElementById("signup_div").style.display = "none";
-    document.getElementById("login_div").style.display = "none";
-
+    dashboardPage();
     var user = firebase.auth().currentUser;
-
     if(user != null){
       var email_id = user.email;
       console.log("Welcome User:", email_id);
     }
-
   } else {
-    // No user is signed in.
-
-    document.getElementById("dashboardPage").style.display = "none";
-    document.getElementById("signup_div").style.display = "none";
-    document.getElementById("login_div").style.display = "block";
+    loginPage();
   }
 });
 
-function loginPage() {
-  document.getElementById("dashboardPage").style.display = "none";
-  document.getElementById("signup_div").style.display = "none";
-  document.getElementById("login_div").style.display = "block";
-}
-
-function signupPage() {
-  document.getElementById("dashboardPage").style.display = "none";
-  document.getElementById("signup_div").style.display = "block";
-  document.getElementById("login_div").style.display = "none";
-}
-
-function login(){
-  var userEmail = document.getElementById("login_email_field").value;
-  var userPass = document.getElementById("login_password_field").value;
-
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-    var loginButton = document.getElementById("login-error-msg");
-    loginButton.style.display = "block";
-    loginButton.innerHTML = "Error: " + errorMessage;
-  });
-}
-
-function loginGoogle(){
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    var token = result.credential.accessToken;
-    var user = result.user;
-  }).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-  });
-}
-
-function signup(){
-  //TODO: Add more features to sign up page
-  var userEmail = document.getElementById("signup_email_field").value;
-  var userPass = document.getElementById("signup_password_field").value;
-  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
-}
-
-function logout(){
-  firebase.auth().signOut();
+function submitAddSite() {
+  firebase.auth().currentUser.getIdToken(true).then(function(idToken) {
+    document.getElementById("user-token").setAttribute("hidden", false);
+    document.getElementById("user-token").value = idToken;
+    document.getElementById("add-site-form").submit();
+  }).catch(function(error) { console.error(error); });
 }
 
 //setInterval(function(){updateDashboard()}, 1000);
-
-function showpasswordmain() {
-  var x = document.getElementById("login_password_field");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
-
-function showpasswordsignup() {
-  var x = document.getElementById("signup_password_field");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
