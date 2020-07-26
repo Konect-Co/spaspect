264f26b9648a413b9c9a6e57b4a7ae8783c0acb8
Ravit Sharma <sharma.ravit@gmail.com>
2020-07-26 16:54:36 -0700

    (Not compiling) Finished modular outline after converting from Python

--- core-cpp/core.cpp
+++ core-cpp/core.cpp
@@ -1,31 +1,31 @@
 // Copyright 2020 The Konect SpaSpect Authors. All Rights Reserved.
-//
 // This file is part of Konect SpaSpect technology.
+//  ___            ___              _   
+// / __|_ __  __ _/ __|_ __  ___ __| |_ 
+// \__ \ '_ \/ _` \__ \ '_ \/ -_) _|  _|
+// |___/ .__/\__,_|___/ .__/\___\__|\__|
+//     |_|            |_|               
+
 
 #include <iostream>
 #include <string>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 
-#include "PixelMapper.h"
-#include "TrackedObject.h"
 #include "utils.h"
 
 using namespace std;
 
 int main() {
-	cv::Mat image;
+	cv::Mat* image;
 
-	cv::VideoCapture cap("/home/ravit/Downloads/TimesSquare.mp4");
-	cap >> image;
+	for (;;) {
+		cv::VideoCapture cap("/home/ravit/Downloads/TimesSquare.mp4");
+		cap >> (*image);
 
-	if(! image.data )
-	{
-		cout <<  "Could not open or find the image" << std::endl;
-		return -1;
-	} else {
-		cout << "Opened image" << std::endl;
-		cv::imwrite("/home/ravit/Pictures/output.png", image);
+		if(! image->data ) {
+			return -1;
+		}
 	}
 
 	return 0;
