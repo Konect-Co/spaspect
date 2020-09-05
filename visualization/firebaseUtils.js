c840aa448b47ed59135df627b0847e20fd8843b3
Cassiano Simas <cassianoss@live.com>
2020-09-05 14:10:51 -0300

    adding fbutils

---
+++ visualization/firebaseUtils.js
@@ -0,0 +1,10 @@
+const { write } = require("fs")
+
+function read(firebaseID, fileLocation) {
+    var db = firebase.databse();
+    db.ref(firebaseID).
+}
+
+function write(firebaseID, fileLocation) {
+
+}
