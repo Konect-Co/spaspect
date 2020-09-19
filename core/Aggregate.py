b3c081af2c1c93c02aad120e78c789896798b065
Ravit Sharma <sharma.ravit@gmail.com>
2020-09-19 14:56:17 -0700

    Aggregate backend working

--- core/Aggregate.py
+++ core/Aggregate.py
@@ -8,6 +8,8 @@ Generates the aggregate data displayed on SpaSpect dashboard
 @return aggregate data
 """
 def genAggData(CVOutput, filename):
+	#TODO: Add a new field every hour
+	# loading aggregate data from file
 	with open(filename, 'r') as f:
 		aggData = json.load(f)
 
@@ -15,17 +17,34 @@ def genAggData(CVOutput, filename):
 	hoursList.sort(reverse=True)
 	currHour = aggData[hoursList[0]]
 
+	currTracked = currHour["currTracked"]
 	tracked = CVOutput["tracked"]
 
-	#TODO: This should be looking at tracked individuals not raw boxes
-	currHour["visitorCount"] += len(CVOutput["distanced"])
-	currHour["undistancedCount"] += 2
-	currHour["unmaskedCount"] += 2
-	currHour["violationsCount"] += 2
-	currHour["averageDistance"] += 0.1
-	currHour["currTracked"] = tracked
+	# looping through the tracked objects
+	for trackedObj in CVOutput["tracked"].values():
+		name = trackedObj["name"]
+		#TODO: Aggregate should perhaps be calculated at the end of each hour
+		if (name not in currTracked):
+			currTracked.append(name)
+			currHour["visitorCount"] += 1
+
+			unmasked = True if trackedObj["masked"] == 2 else False
+			undistanced = True if trackedObj["distanced"] == 0 else False
+			violation = unmasked or undistanced
+
+			if (unmasked):
+				currHour["unmaskedCount"] += 1
+			if (undistanced):
+				currHour["undistancedCount"] += 1
+			if (violation):
+				currHour["violationsCount"] += 1
+
+			#TODO: Handle currHour["averageDistance"]
+
+	currHour["currTracked"] = currTracked
 
 	# have json write this dictionary to file
 	with open(filename, 'w') as f:
 		json.dump(aggData, f, indent=4)
+
 	return aggData
