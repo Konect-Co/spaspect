0fa613a43f72711336a61cff9933221362127e2d
Ravit Sharma <sharma.ravit@gmail.com>
2020-09-27 21:59:05 -0700

    Implemented first function

--- core/socialDistancingEvaluation.py
+++ core/socialDistancingEvaluation.py
@@ -1,16 +1,34 @@
+import cv2
+from cv_model import pred
+
+from utilScripts import obtainStreamLink
+from utilScripts import PixelMapper
+
 import os
 import csv
 import time
 import random
 
 location = "TimesSquare"
+streamWebpage = "https://www.earthcam.com/usa/newyork/timessquare/?cam=tsstreet"
+cap = cv2.VideoCapture()
+
+def getCVOutput():
+	streamLink = obtainStreamLink.get(streamWebpage)
+	
+	cap.open(streamLink)
+	read, image = cap.read()
+	if not read:
+		return None
+	
+	CVOutput = pred.predict(image)
+	return CVOutput
+
+def getNumPeople(CVOutput):
+	return len(CVOutput["boxes"])
 
 #TODO: Implement this
-def getNumPeople():
-	return random.randint(1, 5)
-
-#TODO: Implement this
-def getAverageDistance():
+def getAverageDistance(CVOutput):
 	return random.randint(1, 5)
 
 counter = 0
@@ -20,7 +38,9 @@ with open(os.path.join(os.getcwd(), "csvFiles", location) + ".csv", 'w', newline
 	while True:
 		currTime = time.time()
 
-		num_people = getNumPeople()
+		CVOutput = getCVOutput()
+
+		num_people = getNumPeople(CVOutput)
 		average_distance = getAverageDistance()
 
 		writer.writerow([currTime, num_people, average_distance])
